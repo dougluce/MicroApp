fc6d5e5f3555fa0073a684e15b95bba0fc383f45
Doug Luce <doug@github-microapp.con.com>
2020-12-28 14:06:07 -0800

    Collapse out statusbar
    Just to shorten it a bit.
    
    Remove the accessory activation policy and rely on LSUIElement instead.

--- main.swift
+++ main.swift
@@ -7,17 +7,6 @@ NSApplication.shared.delegate = delegate
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-  var statusBar: StatusBar?
-
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-    NSApp.setActivationPolicy(.accessory)
-    statusBar = StatusBar()
-  }
-}
-
-class StatusBar {
-  private var statusItem: NSStatusItem
-
   lazy var appMenu: NSMenu = {
     let menu = NSMenu()
     let appName = ProcessInfo.processInfo.processName
@@ -28,16 +17,17 @@ class StatusBar {
     menu.addItem(NSMenuItem(title: "Quit \(appName)", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
     return menu
   }()
-  
-  init() {
-    statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-    statusItem.menu = appMenu
-    statusItem.button!.image = NSImage(named: "AppIcon")
-    statusItem.button!.image?.size = NSSize(width: 16.0, height: 16.0)
-  }
+
+  lazy var statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
   @objc private func aboutClicked() {
     NSApp.orderFrontStandardAboutPanel()
     NSApplication.shared.activate(ignoringOtherApps: true)
+  }
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    statusItem.menu = appMenu
+    statusItem.button!.image = NSImage(named: "AppIcon")
+    statusItem.button!.image?.size = NSSize(width: 16.0, height: 16.0)
   }
 }
