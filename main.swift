d6ed7de3ef590f90a87cd4c1073a2c77fdbd480a
Doug Luce <doug@github-microapp.con.com>
2020-12-28 13:44:26 -0800

    Collapse out the status bar menu
    And use the provided routine for termination directly.

--- main.swift
+++ main.swift
@@ -11,68 +11,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     NSApp.setActivationPolicy(.accessory)
-    statusBar = StatusBar.init()
-
-    let appMenuItem = NSMenuItem()
-    appMenuItem.submenu = AppMenu(showKeys: true)
-
-    let mainMenu = NSMenu()
-    mainMenu.addItem(appMenuItem)
-
-    NSApp.mainMenu = mainMenu
+    statusBar = StatusBar()
   }
 }
 
 class StatusBar {
   private var statusItem: NSStatusItem
+
+  lazy var appMenu: NSMenu = {
+    let menu = NSMenu()
+    let appName = ProcessInfo.processInfo.processName
+    let aboutItem = NSMenuItem(title: "About \(appName)", action: #selector(aboutClicked), keyEquivalent: "")
+    aboutItem.target = self
+    menu.addItem(aboutItem)
+    menu.addItem(NSMenuItem.separator())
+    menu.addItem(NSMenuItem(title: "Quit \(appName)", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
+    return menu
+  }()
   
   init() {
     statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-    statusItem.menu = AppMenu(showKeys: false)
-    
-    if let statusBarButton = statusItem.button {
-      statusBarButton.image = (NSImage(named: "AppIcon")!.copy() as! NSImage)
-      statusBarButton.image?.size = NSSize(width: 16.0, height: 16.0)
-    }
+    statusItem.menu = appMenu
+    statusItem.button!.image = NSImage(named: "AppIcon")
+    statusItem.button!.image?.size = NSSize(width: 16.0, height: 16.0)
   }
-}
 
-class AppMenu: NSMenu {
-  var showKeys = true
-
-  required init(coder: NSCoder) {
-    super.init(coder: coder)
-    updateMenu()
-  }
-  
-  init(showKeys: Bool) {
-    self.showKeys = showKeys
-    super.init(title: "MicroApp")
-    updateMenu()
-  }
-  
-  private func updateMenu() {
-    self.removeAllItems()
-    buildMenu()
-  }
-  
-  private func buildMenu() {
-    let appName = ProcessInfo.processInfo.processName
-    let aboutItem = NSMenuItem(title: "About \(appName)", action: #selector(aboutClicked), keyEquivalent: showKeys ? "a" : "")
-    aboutItem.target = self
-    addItem(aboutItem)
-    addItem(NSMenuItem.separator())
-    let quitItem = NSMenuItem(title: "Quit \(appName)", action: #selector(quitClicked), keyEquivalent: showKeys ? "q" : "")
-    quitItem.target = self
-    addItem(quitItem)
-  }
-  
   @objc private func aboutClicked() {
     NSApp.orderFrontStandardAboutPanel()
     NSApplication.shared.activate(ignoringOtherApps: true)
-  }
-  
-  @objc private func quitClicked() {
-    NSApplication.shared.terminate(self)
   }
 }
